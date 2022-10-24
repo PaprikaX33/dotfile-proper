78ec63acb190c2f6d595d7abce0b6654d8f0ccad
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2022-10-24 23:43:50 +0800

    Initial commit of the bootstrapping

---
+++ bootstrap-x33.el
@@ -0,0 +1,25 @@
+;; initialize the melpa, and use-package
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+(provide 'bootstrap-x33)
+
+;; Startup inhibitor
+(setq inhibit-startup-screen t)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+
+;; Start maximize 
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
+;; Font

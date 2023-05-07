ab43563a78cbde491c639d0212ac4cc8021077f3
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-05-07 15:47:41 +0800

    added dockerfile mode

--- load-script/language-x33.el
+++ load-script/language-x33.el
@@ -4,6 +4,10 @@
   :mode (("\\.php$" .  web-mode)
          ("\\.html$" .  web-mode)))
 
+;; Dockerfile
+(use-package dockerfile-mode
+  :ensure t
+  :defer)
 ;; Final provide
 (provide 'language-x33)
 ;; language-x33.el ends here

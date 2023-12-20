a7dc72d8038b2e56bc92544e4f3ac71b488f2851
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-12-20 21:48:20 +0800

    added the checking and loading for machine specific configuration

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 ;; First init file -*- lexical-binding: t; -*-
-;;;(add-to-list 'load-path "load-script")
-;;;(add-to-list 'load-path (concat user-emacs-directory "load-script"))
 (add-to-list 'load-path (expand-file-name "load-script" (file-name-directory load-file-name)))
 (require 'bootstrap-x33)
+;; Load machine specific file
+;; It should be the first after the bootstrap
+;; Since it could be used to alter the other package loaded
+ (when (file-exists-p "~/machine-specific-x33.el")
+   (require 'machine-specific-x33))
 (require 'configuration-x33)
 (require 'buildins-x33)
 (require 'packageing-x33)
